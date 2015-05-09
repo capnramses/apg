c38c7c2d2c691510cc2a8030fb06bf8e584a60b8
capnramses <lists@antongerdelan.net>
2015-05-09 13:25:40 +0100

    log file, param checking, etc.

--- apg.h
+++ apg.h
@@ -14,6 +14,30 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdint.h>
+#include <string.h>
+#include <strings.h> // for strcasecmp
+#include <stdio.h>
+#include <time.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//
+// some stuff for multi-platform builds
+#ifdef WIN32
+#define BUILD_PLAT "win32"
+#elif LINUX32
+#define BUILD_PLAT "linux32"
+#elif LINUX64
+#define BUILD_PLAT "linux64"
+#elif OSX64
+#define BUILD_PLAT "osx64"
+#else // probably this should be more sensible
+#define BUILD_PLAT "undefined"
+#endif
+
+// file name for log
+#define APG_LOG_FN "apg.log"
 
 //
 // replacement for the deprecated min/max functions from original C spec
@@ -48,3 +72,126 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 typedef unsigned int uint;
+
+//
+// like clamp, but if > max then returns min e.g. loops back to 0
+// and if < min, returns max e.g. loops back to 99
+// NOTE: does not preserve differences > 1, so suitable for iterating counters
+// but not big differences
+int loopi (int val, int min, int max);
+
+// checks argv params for a keyword. if so returns index in argv
+// i copied this trick from the DOOM source code
+int check_param (const char* check);
+
+// open/refresh a new log file and print timestamp
+bool restart_apg_log ();
+// write a log entry
+bool apg_log (const char* message, ...);
+// write a log entry and print to stderr
+bool apg_log_err (const char* message, ...);
+
+/*****************************************************************************
+GLOBAL VAR DECLARATIONS (implement these in main.c)
+*****************************************************************************/
+
+extern int g_argc;
+extern char** g_argv;
+
+/*****************************************************************************
+IMPLEMENTATION
+*****************************************************************************/
+
+// NOTE: would it make sense to offset by the difference here, instead of
+// a fixed = max or = min? if so a #define might be better b/c it could apply
+// to more variable types
+inline int loopi (int val, int min, int max) {
+	if (val < min) {
+		return max;
+	}
+	if (val > max) {
+		return min;
+	}
+	return val;
+}
+
+//
+// Checks for given parameter in main's command-line arguments
+// returns the argument number if present (1 to argc - 1)
+// otherwise returns 0
+int check_param (const char* check) {
+  for (int i = 1; i < g_argc; i++) {
+		// NOTE: the original used strcasecmp() here which is the case insenstive
+		// version, but it might require strings.h instead, depending on compiler
+		// it makes sense to ignore case on multi-plat command line
+    if (strcasecmp (check, g_argv[i]) == 0) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+// open/refresh a new log file and print timestamp and OS
+// returns false if could not open file
+inline bool restart_apg_log () {
+	FILE* file = fopen (APG_LOG_FN, "w");
+  if (!file) {
+    fprintf (stderr,
+      "ERROR: could not open APG_LOG_FN log file %s for writing\n",
+      APG_LOG_FN);
+    return false;
+  }
+  time_t now = time (NULL);
+  char* date = ctime (&now);
+  fprintf (file, "APG_LOG_FN log. local time %s\n", date);
+  fprintf (file, "build version: %s %s %s\n", BUILD_PLAT, __DATE__, __TIME__);
+  fclose (file);
+  return true;
+}
+
+// append a log entry
+// returns false if couldn't open file
+inline bool apg_log (const char* message, ...) {
+  va_list argptr;
+  FILE* file = fopen (APG_LOG_FN, "a");
+  if (!file) {
+    fprintf (stderr,
+      "ERROR: could not open APG_LOG_FN %s file for appending\n",
+      APG_LOG_FN);
+    return false;
+  }
+  va_start (argptr, message);
+  vfprintf (file, message, argptr);
+  va_end (argptr);
+  fclose (file);
+  return true;
+}
+
+// append a log entry and print to stderr
+// returns false if couldn't open file
+inline bool apg_log_err (const char* message, ...) {
+	va_list argptr;
+
+	// print to stderr. if log fails at least we still see this
+	va_start (argptr, message);
+  vfprintf (stderr, message, argptr);
+	va_end (argptr);
+
+	// print to log
+  FILE* file = fopen (APG_LOG_FN, "a");
+  if (!file) {
+    fprintf (stderr,
+      "ERROR: could not open APG_LOG_FILE %s file for appending\n",
+      APG_LOG_FN);
+    return false;
+  }
+  va_start (argptr, message);
+  vfprintf (file, message, argptr);
+  va_end (argptr);
+  va_start (argptr, message);
+  va_end (argptr);
+  fclose (file);
+
+  return true;
+}
+
--- test.c
+++ test.c
@@ -1,10 +1,26 @@
 #include "apg.h"
 #include <stdio.h>
 
-int main () {
+int g_argc;
+char** g_argv;
+
+int main (int argc, char** argv) {
+	g_argc = argc;
+	g_argv = argv;
+
+	restart_apg_log ();
+
+	int pn = check_param ("hello");
+	printf ("the user param 'hello' was found at arg num %i\n", pn);
+
 	uint uval = MAX (110, 120);
 	printf ("uval = %u\n", uval);
+	apg_log ("the value was %u\n", uval);
+
 	uval = CLAMP (uval, 11, 12);
 	printf ("uval = %u\n", uval);
+
+	apg_log_err ("ERROR: this is a fake error\n");
+
 	return 0;
 }
