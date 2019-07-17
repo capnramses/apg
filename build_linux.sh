2edd8b7dac4293cf277304348f5d5109e675fc61
Anton Gerdelan <antonofnote@gmail.com>
2019-07-17 13:38:09 +0100

    c99

--- build_linux.sh
+++ build_linux.sh
@@ -5,21 +5,21 @@ set -e
 
 echo "building data structures tests..."
 cd apg_data_structs
-gcc test.c
+gcc -std=c99 test.c
 cd ..
 
 echo "building interpolation tests..."
 cd apg_interp
-gcc test.c -lm
+gcc -std=c99 test.c -lm
 cd ..
 
 echo "building maths library tests..."
 cd apg_maths
-gcc test.c -lm
+gcc -std=c99 test.c -lm
 cd ..
 
 echo "building wav library tests..."
 cd apg_wav
-gcc main_write.c apg_wav.c -lm
-gcc main_read.c apg_wav.c -lm
+gcc -std=c99 main_write.c apg_wav.c -lm
+gcc -std=c99 main_read.c apg_wav.c -lm
 cd ..
