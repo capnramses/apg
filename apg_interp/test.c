151dcf7d850441906b7275c3b7af3325111deb74
Anton Gerdelan <antonofnote@gmail.com>
2019-08-10 22:49:08 +0100

    updated apg_inter prefixes

--- apg_interp/test.c
+++ apg_interp/test.c
@@ -18,19 +18,19 @@ int main() {
     printf( "x = %f\n", x );
 
     float y;
-    y = lerp( x, 0.0f, 20.0f, 0.0f, 1.0f );
+    y = apg_interp_lerp( x, 0.0f, 20.0f, 0.0f, 1.0f );
     printf( "lerp = %f\n", y );
-    y = accel_sine( x, 0.0f, 20.0f, 0.0f, 1.0f );
+    y = apg_interp_accel_sine( x, 0.0f, 20.0f, 0.0f, 1.0f );
     printf( "accel_sine = %f\n", y );
-    y = accel_exp( x, 0.0f, 20.0f, 0.0f, 1.0f );
+    y = apg_interp_accel_exp( x, 0.0f, 20.0f, 0.0f, 1.0f );
     printf( "accel_exp = %f\n", y );
-    y = bow_string( x, 0.0f, 20.0f, 0.0f, 1.0f );
+    y = apg_interp_bow_string( x, 0.0f, 20.0f, 0.0f, 1.0f );
     printf( "bow_string = %f\n", y );
-    y = decel_sine( x, 0.0f, 20.0f, 0.0f, 1.0f );
+    y = apg_interp_decel_sine( x, 0.0f, 20.0f, 0.0f, 1.0f );
     printf( "decel_sine = %f\n", y );
-    y = decel_elastic( x, 0.0f, 20.0f, 0.0f, 1.0f );
+    y = apg_interp_decel_elastic( x, 0.0f, 20.0f, 0.0f, 1.0f );
     printf( "decel_elastic = %f\n", y );
-    y = decel_bounce( x, 0.0f, 20.0f, 0.0f, 1.0f );
+    y = apg_interp_decel_bounce( x, 0.0f, 20.0f, 0.0f, 1.0f );
     printf( "decel_bounce = %f\n", y );
 
     sleep( 1 ); // 1 second
