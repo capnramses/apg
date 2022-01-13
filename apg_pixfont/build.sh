58037b92eb11112b184dca0f98564c95feb02afd
capnramses <antonofnote@gmail.com>
2022-01-13 21:19:55 +0000

    [apg_pixfont] more updates to test and util builds

--- apg_pixfont/build.sh
+++ apg_pixfont/build.sh
@@ -5,4 +5,6 @@ FLAGS="-g -Wall -Wextra -pedantic"
 SAN="" # -fsanitize=address -fsanitize=undefined"
 $CPP $FLAGS $SAN -c tests/test_pixfont.cpp -o tests/test_pixfont.o -I ./ -I ../third_party/stb/
 $CC $FLAGS $SAN -c apg_pixfont.c -o apg_pixfont.o -I ./
-$CPP apg_pixfont.o tests/test_pixfont.o -lm
+$CC -o test_pixfont.bin apg_pixfont.o tests/test_pixfont.o -lm
+
+$CC -o bakefont.bin utils/bake_font_array.c -I ../third_party/stb/ -lm
