a50766e90183112421ad9a745351072e1d86c40f
capnramses <antonofnote@gmail.com>
2022-01-13 21:08:54 +0000

    [apg_wav] updated test program builds with new include path

--- apg_bmp/build.bat
+++ apg_bmp/build.bat
@@ -1,4 +1,4 @@
 REM building apg_bmp tests with clang.exe...
-gcc -g -Wall -Wextra -pedantic -o test_read_bmp.exe test_code/main_read.c -I . -Itest_code/ apg_bmp.c -DAPG_BMP_DEBUG_OUTPUT
-gcc -g -Wall -Wextra -pedantic -o test_write_bmp.exe test_code/main_write.c -I . -Itest_code/ apg_bmp.c -DAPG_BMP_DEBUG_OUTPUT
+gcc -g -Wall -Wextra -pedantic -o test_read_bmp.exe test_code/main_read.c -I . -I test_code/ -I ../third_party/stb/ apg_bmp.c -DAPG_BMP_DEBUG_OUTPUT
+gcc -g -Wall -Wextra -pedantic -o test_write_bmp.exe test_code/main_write.c -I . -I test_code/ -I ../third_party/stb/ apg_bmp.c -DAPG_BMP_DEBUG_OUTPUT
 pause
--- apg_bmp/build.sh
+++ apg_bmp/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-clang -fsanitize=address -fsanitize=undefined -g -Wall -Wextra -pedantic -o test_read_bmp test_code/main_read.c -I./ -Itest_code/ -DAPG_TGA_DEBUG_OUTPUT
-clang -fsanitize=address -fsanitize=undefined -g -Wall -Wextra -pedantic -o test_write_bmp test_code/main_write.c -I./ -Itest_code/ -DAPG_TGA_DEBUG_OUTPUT
+clang -fsanitize=address -fsanitize=undefined -g -Wall -Wextra -pedantic -o test_read_bmp.bin test_code/main_read.c apg_bmp.c -I./ -I test_code/ -I ../third_party/stb/ -DAPG_TGA_DEBUG_OUTPUT
+clang -fsanitize=address -fsanitize=undefined -g -Wall -Wextra -pedantic -o test_write_bmp.bin test_code/main_write.c apg_bmp.c -I./ -I test_code/ -I ../third_party/stb/ -DAPG_TGA_DEBUG_OUTPUT
