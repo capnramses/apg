819dea906375da24da707b204dcef91a3fde2874
capnramses <antonofnote@gmail.com>
2022-01-13 21:23:53 +0000

    [apg_tga] fixed up build scripts for tests

--- apg_tga/build.sh
+++ apg_tga/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-clang -fsanitize=address -fsanitize=undefined -g -Wall -Wextra -pedantic -o test_read_bmp test_code/main_read.c -I./ -Itest_code/ apg_bmp.c -DAPG_BMP_DEBUG_OUTPUT
-clang -fsanitize=address -fsanitize=undefined -g -Wall -Wextra -pedantic -o test_write_bmp test_code/main_write.c -I./ -Itest_code/ apg_bmp.c -DAPG_BMP_DEBUG_OUTPUT
+clang -fsanitize=address -fsanitize=undefined -g -Wall -Wextra -pedantic -o test_read_tga.bin test_code/main_read.c -I ./ -I ../third_party/stb/ -DAPG_TGA_DEBUG_OUTPUT
+clang -fsanitize=address -fsanitize=undefined -g -Wall -Wextra -pedantic -o test_write_tga.bin test_code/main_write.c -I ./ -I ../third_party/stb/ -DAPG_TGA_DEBUG_OUTPUT
