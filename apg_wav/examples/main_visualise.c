3fc89e352e675a8eb91d20b8e1ca5a910381542a
capnramses <antonofnote@gmail.com>
2022-07-24 16:56:44 +0100

    [apg_wav] waveform bitmap creation using apg_plot

--- apg_wav/examples/main_visualise.c
+++ apg_wav/examples/main_visualise.c
@@ -1,12 +1,20 @@
-// Example that plays a .wav file
-// For this example install the PortAudio library first:
-// http://portaudio.com/
-// Ubuntu: `sudo apt install portaudio19-dev`
-// Compile e.g.:
-// gcc examples/main_play_file.c apg_wav.c -o test_play_file.bin -I ./ -lm -lportaudio
+/** Example program that plays a sound and also draws the waveform into an image file `out_vis.png`.
+ *
+ * For this example install the PortAudio library first:
+ * http://portaudio.com/
+ * Ubuntu: `sudo apt install portaudio19-dev`
+
+Compile e.g.:
+
+  gcc examples\main_visualise.c apg_wav.c ..\apg_plot\apg_plot.c -o test_visualise_file.exe
+  -I . -I ..\apg_plot\ -I ..\third_party\ -I ..\third_party\portaudio\include\ -lm -lportaudio -L .
+*/
 
 #include "portaudio.h"
 #include "apg_wav.h"
+#include "apg_plot.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -43,6 +51,44 @@ static int antons_pa_cb( const void* input_buffer_ptr, void* output_buffer_ptr, 
   return 0;
 }
 
+static void _wav_bitmap( apg_wav_t* wav_ptr ) {
+  // NOTE--ignoring n_chans here, so won't work for 2-channel wavs
+
+  uint32_t actual_data_sz = wav_ptr->header_ptr->file_sz - 44; // 44 is size of WAV PCM header.
+  uint32_t sample_sz      = wav_ptr->header_ptr->bits_per_sample / 8;
+  uint32_t n_samples      = actual_data_sz / sample_sz;
+  float duration_s        = apg_wav_duration( wav_ptr );
+  int img_width           = (int)( duration_s * 512.0f );
+  int img_height          = 256;
+
+  apg_plot_t chart = apg_plot_init( ( apg_plot_params_t ){ .h = img_height, .w = img_width, .max_y = 1.0f, .min_y = -1.0f, .max_x = n_samples, .min_x = 0 } );
+  apg_plot_background_colour( 0xCC );
+  apg_plot_line_colour( 0x00, 0x00, 0xFF );
+  apg_plot_x_axis_colour( 0, 0, 0 );
+  apg_plot_clear( &chart );
+
+  float* xy_ptr = malloc( n_samples * wav_ptr->header_ptr->bits_per_sample * 2.0f );
+  if ( !xy_ptr ) { return; }
+
+  for ( uint32_t i = 0; i < n_samples; i++ ) {
+    xy_ptr[i * 2 + 0] = (float)i;
+    if ( 1 == sample_sz ) { // uint8_t
+      xy_ptr[i * 2 + 1] = (float)wav_ptr->pcm_data_ptr[i * sample_sz] / 127.5f - 1.0f;
+    } else if ( 2 == sample_sz ) { // int16_t signed
+      int16_t sample = 0;
+      memcpy( &sample, &wav_ptr->pcm_data_ptr[i * sample_sz], sample_sz );
+      xy_ptr[i * 2 + 1] = (float)sample / 32768.0f;
+    }
+  }
+
+  apg_plot_plot_lines( &chart, xy_ptr, n_samples );
+  apg_plot_x_axis_draw( &chart, 0.0f );
+  stbi_write_png( "out_vis.png", img_width, img_height, 3, chart.rgb_ptr, 3 * img_width );
+
+  free( xy_ptr );
+  apg_plot_free( &chart );
+}
+
 int main( int argc, char** argv ) {
   if ( argc < 2 ) {
     printf( "usage: %s FILE.wav\n", argv[0] );
@@ -60,6 +106,8 @@ int main( int argc, char** argv ) {
   }
   audio_source.duration_s = apg_wav_duration( &audio_source.wav );
 
+  _wav_bitmap( &audio_source.wav );
+
   // Start PortAudio
   printf( "%s\n", Pa_GetVersionText() );
   PaError err = Pa_Initialize();
@@ -68,6 +116,7 @@ int main( int argc, char** argv ) {
     return 1;
   }
   printf( "BPS = %i\n", (int)audio_source.wav.header_ptr->bits_per_sample );
+  printf( "#chans = %i\n", (int)audio_source.wav.header_ptr->n_chans );
 
   PaSampleFormat fmt = paFloat32;
   switch ( audio_source.wav.header_ptr->bits_per_sample ) {
@@ -81,12 +130,12 @@ int main( int argc, char** argv ) {
 
   PaStream* stream = NULL; // usually just 1 stream per device
   err              = Pa_OpenDefaultStream( &stream,
-    0,                                    // no input channels (mic/record etc)
-    audio_source.wav.header_ptr->n_chans, // mono/stereo
-    fmt,                                  // 8-bit, 16-bit int or 32-bit float supported in this demo
-    audio_source.wav.header_ptr->sample_rate_hz,
-    256, // frames per buffer to request from callback (can use paFramesPerBufferUnspecified)
-    antons_pa_cb, &audio_source );
+                 0,                                    // no input channels (mic/record etc)
+                 audio_source.wav.header_ptr->n_chans, // mono/stereo
+                 fmt,                                  // 8-bit, 16-bit int or 32-bit float supported in this demo
+                 audio_source.wav.header_ptr->sample_rate_hz,
+                 256, // frames per buffer to request from callback (can use paFramesPerBufferUnspecified)
+                 antons_pa_cb, &audio_source );
   if ( err != paNoError ) {
     printf( "PortAudio error: %s\n", Pa_GetErrorText( err ) );
     return 1;
