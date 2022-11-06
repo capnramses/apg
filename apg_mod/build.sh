dc6623f53336cab6f4589ada23b5105dccbdb529
capnramses <antonofnote@gmail.com>
2022-11-06 17:30:01 +0000

    [apg_mod] playsong added to build.sh

--- apg_mod/build.sh
+++ apg_mod/build.sh
@@ -4,6 +4,8 @@ SANS="-fsanitize=thread -fsanitize=undefined"
 FLAGS="-Wall -Wextra -pedantic"
 clang -o print_tracks.bin $SANS $FLAGS tests/print_tracks.c apg_mod.c -I ./
 
+clang -o play_song.bin $SANS $FLAGS examples/play_song.c apg_mod.c -I ./ -lportaudio
+
 APG_WAV_INC="../apg_wav/"
 
 clang -o dump_wavs.bin $SANS $FLAGS tests/dump_wavs.c apg_mod.c -I ./ \
