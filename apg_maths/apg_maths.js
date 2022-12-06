e78191424b84fbd5ec85e4423bc4789a730790c7
capnramses <antonofnote@gmail.com>
2022-12-06 16:52:54 +0000

    -not tested yet- update to apg_math.js

--- apg_maths/apg_maths.js
+++ apg_maths/apg_maths.js
@@ -1,32 +1,34 @@
-//
-// 3d maths lib (semi-finished port of my C++ maths library)
-// Licence: see bottom of file.
-// anton gerdelan
-// 23 dec 2014
-// antongerdelan.net
-// 19 mar 2015 - patched by emma carrigan (quat_quat_mult, +scale mat4)
-// 24 mar 2015 - added look_at(). swapped argument order in mult_quat_quat()
-// and transposed the multiplications in mult_mat4_vec4() for column-major
-// 15 may 2015 - added rotate_z_deg(), length2
-// 23 jun 2015 - dot_versor() and slerp() added
-//
+/* JavaScript 3D maths library.
+	 
+   Original Author: Anton Gerdelan - antongerdelan.net
+	 Licence:         see bottom of file.
+	 
+	 Version History & Contributors:
+		 06 Dec 2022 - Anton Gerdelan - "mult_" -> "mul". Updated some comment documentation.
+									                  API-breaking change - matrix scale/rotate/translation functions no longer accept another matrix as an argument, and function names include "mat4".
+		 23 jun 2015 - Anton Gerdelan - dot_versor() and slerp() added.
+		 15 may 2015 - Anton Gerdelan - Added rotate_z_deg(), length2().
+		 24 mar 2015 - Anton Gerdelan - Added look_at(). swapped argument order in mul_quat_quat() and transposed the multiplications in mul_mat4_vec4() for column-major.
+		 19 mar 2015 - Emma Carrigan  - Fixed bugs in quat_quat_mult, scale_mat4.
+	   23 dec 2014 - Anton Gerdelan - First conversion from C++ maths library.
+*/
 
 /*--------------------------------CONSTANTS----------------------------------*/
 
 var TAU = 2.0 * Math.PI;
-var ONE_DEG_IN_RAD = (2.0 * Math.PI) / 360.0; // 0.017444444
-var ONE_RAD_IN_DEG = 360.0 / (2.0 * Math.PI); //57.2957795
+var ONE_DEG_IN_RAD = (2.0 * Math.PI) / 360.0; //  0.017444444
+var ONE_RAD_IN_DEG = 360.0 / (2.0 * Math.PI); // 57.2957795
 
 /*-----------------------------VECTOR FUNCTIONS------------------------------*/
 
-function mult_vec2_scal(s, x) {
+function mul_vec2_scal(s, x) {
 	var v = new Array();
 	v[0] = s[0] * x;
 	v[1] = s[1] * x;
 	return v;
 }
 
-function mult_vec3_scal(s, x) {
+function mul_vec3_scal(s, x) {
 	var v = new Array();
 	v[0] = s[0] * x;
 	v[1] = s[1] * x;
@@ -64,14 +66,14 @@ function sub_vec3_vec3(s, t) {
 	return v;
 }
 
-function mult_vec2_vec2(s, t) {
+function mul_vec2_vec2(s, t) {
 	var v = new Array();
 	v[0] = s[0] * t[0];
 	v[1] = s[1] * t[1];
 	return v;
 }
 
-function mult_vec3_vec3(s, t) {
+function mul_vec3_vec3(s, t) {
 	var v = new Array();
 	v[0] = s[0] * t[0];
 	v[1] = s[1] * t[1];
@@ -79,16 +81,20 @@ function mult_vec3_vec3(s, t) {
 	return v;
 }
 
+function length2_vec2(v) {
+	return v[0] * v[0] + v[1] * v[1];
+}
+
 function length_vec2(v) {
 	return Math.sqrt(v[0] * v[0] + v[1] * v[1]);
 }
 
-function length_vec3(v) {
-	return Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-}
-
 function length2_vec3(v) {
 	return v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
+}
+
+function length_vec3(v) {
+	return Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
 function normalise_vec2(v) {
@@ -146,7 +152,7 @@ function zero_mat4() {
 //
 // multiply 2 4x4 matrices together
 //
-function mult_mat4_mat4(a, b) {
+function mul_mat4_mat4(a, b) {
 	var r = [
 		0.0, 0.0, 0.0, 0.0,
 		0.0, 0.0, 0.0, 0.0,
@@ -171,7 +177,7 @@ function mult_mat4_mat4(a, b) {
 // multiply 4d vector with 4x4 matrix. return resultant 4d vector
 // 24 mar 2015 - anton - found out this was transposed and fixed it
 //
-function mult_mat4_vec4(m, v) {
+function mul_mat4_vec4(m, v) {
 	var x = m[0] * v[0] + m[4] * v[1] + m[8] * v[2] + m[12] * v[3];
 	var y = m[1] * v[0] + m[5] * v[1] + m[9] * v[2] + m[13] * v[3];
 	var z = m[2] * v[0] + m[6] * v[1] + m[10] * v[2] + m[14] * v[3];
@@ -219,9 +225,7 @@ function determinant(m) {
 //
 function inverse_mat4(m) {
 	var det = determinant(m);
-	//console.log ("det = " + det);
-	// there is no inverse if determinant is zero (not likely unless scale is
-	// broken)
+	// there is no inverse if determinant is zero (not likely unless scale is broken)
 	if (0.0 == det) {
 		console.error("WARNING. matrix has no determinant. can not invert\n");
 		return m;
@@ -317,7 +321,7 @@ function inverse_mat4(m) {
 }
 
 //
-// print the contents of a 4x4 matrix in a readible way
+// print the contents of a 4x4 matrix in a readable way
 //
 function print_mat4(m) {
 	console.log("mat4:");
@@ -333,87 +337,74 @@ function print_mat4(m) {
 
 /*---------------------------AFFINE TRANSFORMS-------------------------------*/
 
-//
-// translate a 4d matrix with xyz array
-//
-function translate_mat4(m, v) {
-	var m_t = identity_mat4();
-	m_t[12] = v[0];
-	m_t[13] = v[1];
-	m_t[14] = v[2];
-	return mult_mat4_mat4(m_t, m);
+// Returns a 4x4 column-major translation matrix.
+//   xyz - 3-value array representing a 3D vector. e.g. [0,2,6].
+function translate_mat4(xyz) {
+	var mat = identity_mat4();
+	mat[12] = xyz[0];
+	mat[13] = xyz[1];
+	mat[14] = xyz[2];
+	return mat;
 }
 
-//
-// rotate around x axis by an angle in degrees
-//
-function rotate_x_deg(m, deg) {
-	// convert to radians
-	var rad = deg * ONE_DEG_IN_RAD;
-	var m_r = identity_mat4();
-	m_r[5] = Math.cos(rad);
-	m_r[9] = -Math.sin(rad);
-	m_r[6] = Math.sin(rad);
-	m_r[10] = Math.cos(rad);
-	return mult_mat4_mat4(m_r, m);
+// Returns a 4x4 column-major matrix for a rotation about the X-axis.
+//   deg - An angle in degrees to rotate about. e.g. -1.3
+function rotate_mat4_x_deg(deg) {
+	var rad = deg * ONE_DEG_IN_RAD; // Convert to radians.
+	var mat = identity_mat4();
+	mat[5] = Math.cos(rad);
+	mat[9] = -Math.sin(rad);
+	mat[6] = Math.sin(rad);
+	mat[10] = Math.cos(rad);
+	return mat;
 }
 
-//
-// rotate around y axis by an angle in degrees
-//
-function rotate_y_deg(m, deg) {
-	// convert to radians
-	var rad = deg * ONE_DEG_IN_RAD;
-	var m_r = identity_mat4();
-	m_r[0] = Math.cos(rad);
-	m_r[8] = Math.sin(rad);
-	m_r[2] = -Math.sin(rad);
-	m_r[10] = Math.cos(rad);
-	return mult_mat4_mat4(m_r, m);
+// Returns a 4x4 column-major matrix for a rotation about the Y-axis.
+//   deg - An angle in degrees to rotate about. e.g. -1.3
+function rotate_mat4_y_deg(deg) {
+	var rad = deg * ONE_DEG_IN_RAD; // Convert to radians.
+	var mat = identity_mat4();
+	mat[0] = Math.cos(rad);
+	mat[8] = Math.sin(rad);
+	mat[2] = -Math.sin(rad);
+	mat[10] = Math.cos(rad);
+	return mat;
 }
 
-//
-// rotate around z axis by an angle in degrees
-//
-function rotate_z_deg(m, deg) {
-	// convert to radians
-	var rad = deg * ONE_DEG_IN_RAD;
-	var m_r = identity_mat4();
-	m_r[0] = Math.cos(rad);
-	m_r[4] = -Math.sin(rad);
-	m_r[1] = Math.sin(rad);
-	m_r[5] = Math.cos(rad);
-	return mult_mat4_mat4(m_r, m);
+// Returns a 4x4 column-major matrix for a rotation about the Z-axis.
+//   deg - An angle in degrees to rotate about. e.g. -1.3
+function rotate_mat4_z_deg(deg) {
+	var rad = deg * ONE_DEG_IN_RAD; // Convert to radians.
+	var mat = identity_mat4();
+	mat[0] = Math.cos(rad);
+	mat[4] = -Math.sin(rad);
+	mat[1] = Math.sin(rad);
+	mat[5] = Math.cos(rad);
+	return mat;
 }
 
-//
-// scale a matrix by [x, y, z]
-//
-function scale_mat4(m, v) {
-	var a = identity_mat4();
-	a[0] = v[0];
-	a[5] = v[1];
-	a[10] = v[2];
-	return mult_mat4_mat4(a, m);
+// Returns a 4x4 matrix for scaling, based on 3D vector xyz.
+function scale_mat4(xyz) {
+	var mat = identity_mat4();
+	mat[0] = xyz[0];
+	mat[5] = xyz[1];
+	mat[10] = xyz[2];
+	return mat;
 }
 
 /*-----------------------------VIRTUAL CAMERA--------------------------------*/
 
 //
-// generate a view matrix in gluLookAt() style
-//
+// Returns a 4x4 column-major virtual camera "view" matrix in gluLookAt() style.
+//   cam_pos  - Camera position in world units as a 3D array. e.g. [0.0, 0.0, 0.0] to place at the world origin..
+//   targ_pos - Camera focus point in world units as a 3D array. e.g. [0.0, 0.0, -10.0] to gaze down the -Z axis.
+//   up       - Camera vertical direction in world coordinates as a 3D array. e.g. [0.0, 1.0, 0.0] for orientation with no roll or pitch.
 function look_at(cam_pos, targ_pos, up) {
-	// inverse translation
-	var p = identity_mat4();
-	p = translate_mat4(p, [-cam_pos[0], -cam_pos[1], -cam_pos[2]]);
-	// distance vector
-	var d = sub_vec3_vec3(targ_pos, cam_pos);
-	// forward vector
-	var f = normalise_vec3(d);
-	// right vector
-	var r = normalise_vec3(cross_vec3(f, up));
-	// real up vector
-	var u = normalise_vec3(cross_vec3(r, f));
+	var p = translate_mat4([-cam_pos[0], -cam_pos[1], -cam_pos[2]]); // Inverse translation.
+	var d = sub_vec3_vec3(targ_pos, cam_pos); // Distance vector.
+	var f = normalise_vec3(d); 	// Forward vector.
+	var r = normalise_vec3(cross_vec3(f, up)); // Right vector.
+	var u = normalise_vec3(cross_vec3(r, f)); // Real up vector.
 	var ori = identity_mat4();
 	ori[0] = r[0];
 	ori[4] = r[1];
@@ -424,13 +415,14 @@ function look_at(cam_pos, targ_pos, up) {
 	ori[2] = -f[0];
 	ori[6] = -f[1];
 	ori[10] = -f[2];
-
-	return mult_mat4_mat4(ori, p);
+	return mul_mat4_mat4(ori, p);
 }
 
-//
-// generate 4x4 perspective projection matrix
-//
+// Returns a 4x4 column-major perspective projection matrix.
+//   fovy   - Vertical field of view angle in degrees. e.g. 66.6.
+//   aspect - Aspect ratio. e.g. viewport width / height.
+//   near   - Near clip plane distance. e.g. 0.1.
+//   far    - Far clip plane distance. e.g. 1000.0.
 function perspective(fovy, aspect, near, far) {
 	var fov_rad = fovy * ONE_DEG_IN_RAD;
 	var range = Math.tan(fov_rad / 2.0) * near;
@@ -449,9 +441,7 @@ function perspective(fovy, aspect, near, far) {
 
 /*--------------------------HAMILTON'S QUATERNIONS---------------------------*/
 
-//
-// generate versor from an angle in radians and axis
-//
+// Returns a versor from an angle in radians and an axis.
 function quat_from_axis_rad(radians, x, y, z) {
 	var result = new Array();
 	result[0] = Math.cos(radians / 2.0);
@@ -461,19 +451,15 @@ function quat_from_axis_rad(radians, x, y, z) {
 	return result;
 }
 
-//
-// generate versor from an angle in degrees and axis
-//
+// Returns a versor from an angle in degrees and an axis.
 function quat_from_axis_deg(degrees, x, y, z) {
 	return quat_from_axis_rad(ONE_DEG_IN_RAD * degrees, x, y, z);
 }
 
-//
-// order of params was confusing so swapped around 24 mar 2015
-// it's now quat = mult (parent, child)
-// it was quat = mult (child, parent)
-//
-function mult_quat_quat(s, r) {
+// Returns a quaternion, produced as a multiplication of two quaterions s, and r.
+// e.g. For use in e.g. skeletal animation:
+//   var result_quat = mul_quat_quat(parent_orientation_quat, child_orientation_quat).
+function mul_quat_quat(s, r) {
 	var t = new Array();
 	t[0] = r[0] * s[0] - r[1] * s[1] - r[2] * s[2] - r[3] * s[3];
 	t[1] = r[0] * s[1] + r[1] * s[0] - r[2] * s[3] + r[3] * s[2];
@@ -482,9 +468,7 @@ function mult_quat_quat(s, r) {
 	return t;
 }
 
-//
-// convert a versor to a 4x4 matrix for use in shaders
-//
+// Returns a column-major 4x4 matrix form from a versor.
 function quat_to_mat4(q) {
 	var w = q[0];
 	var x = q[1];
@@ -510,47 +494,40 @@ function quat_to_mat4(q) {
 	return m;
 }
 
+// Returns the dot product of two versors q, and r.
 function dot_versor(q, r) {
 	return q[0] * r[0] + q[1] * r[1] + q[2] * r[2] + q[3] * r[3];
 }
 
-// spherical linear interpolation between two quaternions
-// factor t between 0.0 and 1.0
-// returns interpolated versor
+// Spherical linear interpolation between two quaternions.
+// Returns an interpolated versor.
+//   q - The versor to interpolate from.
+//   r - The versor to interpolate to.
+//   t - A factor of interpolation between q, and r, of between 0.0 and 1.0.
 function slerp(q, r, t) {
-	// angle between q0-q1
-	var cos_half_theta = dot_versor(q, r);
-	// as found here http://stackoverflow.com/questions/2886606/flipping-issue-when-interpolating-rotations-using-quaternions
-	// if dot product is negative then one quaternion should be negated, to make
-	// it take the short way around, rather than the long way
-	// yeah! and furthermore Susan, I had to recalculate the d.p. after this
+	var cos_half_theta = dot_versor(q, r); // Angle between q0-q1.
+	// As found here http://stackoverflow.com/questions/2886606/flipping-issue-when-interpolating-rotations-using-quaternions
+	// If dot product is negative then one quaternion should be negated, to make it take the short way around, rather than the long way.
+	// This requires also recalculating the dot product.
 	if (cos_half_theta < 0.0) {
-		for (var i = 0; i < 4; i++) {
-			q[i] *= -1.0;
-		}
+		for (var i = 0; i < 4; i++) { q[i] *= -1.0; }
 		cos_half_theta = dot_versor(q, r);
 	}
-	// if qa=qb or qa=-qb then theta = 0 and we can return qa
-	if (Math.abs(cos_half_theta) >= 1.0) {
-		return q;
-	}
+	// If qa=qb or qa=-qb then theta = 0 and we can return qa.
+	if (Math.abs(cos_half_theta) >= 1.0) { return q; }
 	// Calculate temporary values
 	var sin_half_theta = Math.sqrt(1.0 - cos_half_theta * cos_half_theta);
-	// if theta = 180 degrees then result is not fully defined
-	// we could rotate around any axis normal to qa or qb
+	// If theta == 180 degrees then the result is not fully defined.
+	// We could rotate around any axis normal to qa or qb.
 	var result = [];
 	if (Math.abs(sin_half_theta) < 0.001) {
-		for (var i = 0; i < 4; i++) {
-			result[i] = (1.0 - t) * q[i] + t * r[i];
-		}
+		for (var i = 0; i < 4; i++) { result[i] = (1.0 - t) * q[i] + t * r[i]; }
 		return result;
 	}
 	var half_theta = Math.acos(cos_half_theta);
 	var a = Math.sin((1.0 - t) * half_theta) / sin_half_theta;
 	var b = Math.sin(t * half_theta) / sin_half_theta;
-	for (var i = 0; i < 4; i++) {
-		result[i] = q[i] * a + r[i] * b;
-	}
+	for (var i = 0; i < 4; i++) { result[i] = q[i] * a + r[i] * b; }
 	return result;
 }
 
