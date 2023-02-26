3d2f0a1264f6407b18c212304e5681f5efd6183e
capnramses <antonofnote@gmail.com>
2023-02-26 18:12:19 +0000

    [apg_pixelfont] atlas creation util

---
+++ apg_pixfont/utils/create_atlas.c
@@ -0,0 +1,79 @@
+// Example utility to create a typical 16x16 cell atlas image using apg_pixelfont
+// by Anton Gerdelan 2023 Feb 26.
+
+#include "apg_pixfont.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  const char* atlas_str = "atlas.png";
+  // Find max dimensions per character and check that font will fit in atlas.
+  int cell_dims[2] = { 16, 16 }; // ~1MB per 256-char sheet. Prefer power-of-two sizes, but not essential.
+  int padding_px   = 4;
+  int thickness    = 1;
+  int add_outline  = 1;
+  int max_w = 0, max_h = 0;
+  for ( int i = 0; i < 256; i++ ) {
+    char tmp[2] = { i, '\0' };
+    int w = 0, h = 0;
+    if ( !apg_pixfont_image_size_for_str( tmp, &w, &h, thickness, add_outline, 0 ) ) { continue; }
+    max_w = w < max_w ? max_w : w;
+    max_h = h < max_h ? max_h : h;
+  }
+  printf( "max_w,h = {%i,%i}\n", max_w, max_h );
+  if ( max_w + padding_px > cell_dims[0] || max_h + padding_px > cell_dims[1] ) {
+    fprintf( stderr, "ERROR: cells are not big enough for glyphs. Resize.\n" );
+    // return 1;
+  }
+  int n_chans        = 4; // Could be 2, or even 1, if there are no outlines.
+  uint8_t* atlas_ptr = calloc( cell_dims[0] * cell_dims[1] * n_chans, 256 );
+  if ( !atlas_ptr ) {
+    fprintf( stderr, "ERROR: malloc OOM\n" );
+    return 1;
+  }
+  size_t subimg_sz    = cell_dims[0] * cell_dims[1] * n_chans;
+  uint8_t* subimg_ptr = calloc( subimg_sz, 1 );
+  if ( !subimg_ptr ) {
+    fprintf( stderr, "ERROR: malloc OOM\n" );
+    return 1;
+  }
+  for ( int i = 0; i < 256; i++ ) {
+    memset( subimg_ptr, 0, subimg_sz );
+    char tmp[5]  = { i, '\0' };
+    int cell_col = i % 16;
+    int cell_row = i / 16;
+    // Latin-1 Supplement.
+    if ( i >= 160 ) {
+      tmp[0] = 0xC3;
+      tmp[1] = i - 160 + 0x81 - 33;
+      tmp[2] = 0x00;
+    }
+    if ( !apg_pixfont_str_into_image( tmp, subimg_ptr, cell_dims[0], cell_dims[1], n_chans, 0xFF, 0xFF, 0xFF, 0xFF, thickness, add_outline, 0 ) ) {
+      fprintf( stderr, "Bad result writing image for char %i '%c'\n", i, tmp[0] );
+      continue;
+    }
+    char subimg_str[64];
+    sprintf( subimg_str, "images/%i.png", i );
+    // stbi_write_png( subimg_str, cell_dims[0], cell_dims[1], n_chans, subimg_ptr, cell_dims[0] * n_chans );
+
+    size_t cell_row_offset = cell_row * 16 * cell_dims[0] * cell_dims[1] * n_chans;
+    size_t cell_col_offset = cell_col * cell_dims[0] * n_chans;
+    for ( int y = 0; y < cell_dims[1]; y++ ) { // Copy one sub-image line at a time.
+      uint8_t* dst_ptr = atlas_ptr + cell_row_offset + cell_col_offset + y * 16 * cell_dims[0] * n_chans;
+      uint8_t* src_ptr = subimg_ptr + cell_dims[0] * n_chans * y;
+      memcpy( dst_ptr, src_ptr, cell_dims[0] * n_chans );
+    }
+  }
+  printf( "Writing atlas.png\n" );
+  int ret = stbi_write_png( "atlas.png", cell_dims[0] * 16, cell_dims[1] * 16, n_chans, atlas_ptr, cell_dims[0] * 16 * n_chans );
+  fprintf( stderr, "%i writing atlas\n", ret );
+
+  free( subimg_ptr );
+  free( atlas_ptr );
+  printf( "done\n" );
+  return 0;
+}
