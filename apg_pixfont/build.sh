f68b0d77d7c21fb5201bcb806b61aa98843a660f
capnramses <antonofnote@gmail.com>
2023-03-08 16:59:20 +0000

    test build info

--- apg_pixfont/build.sh
+++ apg_pixfont/build.sh
@@ -1,3 +1,14 @@
+#!/bin/sh
+
+# any error code causes script to exit with error code
+set -e
+# echo everything
+#set -x
+
+colour_pass="\033[32;1m"
+colour_default="\033[0m"
+horiz_div="========================"
+
 CC=clang
 CPP=clang++
 LD=ld
@@ -8,3 +19,5 @@ $CC $FLAGS $SAN -c apg_pixfont.c -o apg_pixfont.o -I ./
 $CC -o test_pixfont.bin apg_pixfont.o tests/test_pixfont.o -lm
 
 $CC -o bakefont.bin utils/bake_font_array.c -I ../third_party/stb/ -lm
+
+printf '%b%s\n%s\n%s\n%b\n' $colour_pass $horiz_div "Build SUCCESS" $horiz_div $colour_default
