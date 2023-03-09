d4e20c4de9f907600193ad52246ba8f54908eb3d
capnramses <antonofnote@gmail.com>
2023-03-09 13:17:08 +0000

    builds

---
+++ apg_pixfont/build.bat
@@ -0,0 +1,12 @@
+set CC=gcc
+set CPP=g++
+set LD=ld
+set FLAGS=-g -Wall -Wextra -pedantic -Wno-missing-field-initializers
+set SAN=
+REM # -fsanitize=address -fsanitize=undefined"
+%CPP% %FLAGS% %SAN% -c tests/test_pixfont.cpp -o build/test_pixfont.o -I . -I ../third_party/stb/
+%CC% %FLAGS% %SAN% -c apg_pixfont.c -o build/apg_pixfont.o -I .
+%CPP% -o test_pixfont.exe build/apg_pixfont.o build/test_pixfont.o -lm
+
+%CC% -o bakefont.exe utils/bake_font_array.c -I ../third_party/stb/ -lm
+%CC% -o create_atlas.exe utils/create_atlas.c apg_pixfont.c -I ../third_party/stb/ -I . -lm
--- apg_pixfont/build.sh
+++ apg_pixfont/build.sh
@@ -12,12 +12,13 @@ horiz_div="========================"
 CC=clang
 CPP=clang++
 LD=ld
-FLAGS="-g -Wall -Wextra -pedantic"
+FLAGS="-g -Wall -Wextra -pedantic -Wno-missing-field-initializers"
 SAN="" # -fsanitize=address -fsanitize=undefined"
-$CPP $FLAGS $SAN -c tests/test_pixfont.cpp -o tests/test_pixfont.o -I ./ -I ../third_party/stb/
-$CC $FLAGS $SAN -c apg_pixfont.c -o apg_pixfont.o -I ./
-$CC -o test_pixfont.bin apg_pixfont.o tests/test_pixfont.o -lm
+$CPP $FLAGS $SAN -c tests/test_pixfont.cpp -o build/test_pixfont.o -I ./ -I ../third_party/stb/
+$CC $FLAGS $SAN -c apg_pixfont.c -o build/apg_pixfont.o -I ./
+$CPP -o test_pixfont.bin build/apg_pixfont.o build/test_pixfont.o -lm
 
 $CC -o bakefont.bin utils/bake_font_array.c -I ../third_party/stb/ -lm
+$CC -o create_atlas.bin utils/create_atlas.c apg_pixfont.c -I ../third_party/stb/ -I ./ -lm
 
 printf '%b%s\n%s\n%s\n%b\n' $colour_pass $horiz_div "Build SUCCESS" $horiz_div $colour_default
