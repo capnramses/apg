cbc07e20953e4a7eec41e24c3a38992e8a412e79
Anton Gerdelan <1935602+capnramses@users.noreply.github.com>
2025-06-15 15:53:31 +0100

    add bmp to pxfont build

--- apg_pixfont/build.sh
+++ apg_pixfont/build.sh
@@ -9,6 +9,9 @@ colour_pass="\033[32;1m"
 colour_default="\033[0m"
 horiz_div="========================"
 
+cp ../apg_bmp/apg_bmp.c ./utils/
+cp ../apg_bmp/apg_bmp.h ./utils/
+
 CC=clang
 CPP=clang++
 LD=ld
@@ -19,6 +22,6 @@ $CC $FLAGS $SAN -c apg_pixfont.c -o build/apg_pixfont.o -I ./
 $CPP -o test_pixfont.bin build/apg_pixfont.o build/test_pixfont.o -lm
 
 $CC -o bakefont.bin utils/bake_font_array.c -I ../third_party/stb/ -lm
-$CC -o create_atlas.bin utils/create_atlas.c apg_pixfont.c -I ../third_party/stb/ -I ./ -lm
+$CC -o create_atlas.bin utils/create_atlas.c utils/apg_bmp.c apg_pixfont.c -I ../third_party/stb/ -I ./ -lm
 
 printf '%b%s\n%s\n%s\n%b\n' $colour_pass $horiz_div "Build SUCCESS" $horiz_div $colour_default
